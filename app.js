266df1dfba460d076cca04f2080839ebfafbe964
AaronBruce <aaron.bruce@comcast.net>
2017-06-13 06:26:26 -0700

    created scaffold for files

---
+++ app.js
@@ -0,0 +1,116 @@
+'use strict'
+
+// var maddy = {
+//   nickname: 'Maddy',
+//   givenName: 'Madeline',
+//   personality: ['fun', 'helpful', 'awesome'],
+//   greeting: function() {
+//     console.log('Hi,  my name is ' + this.givenName + ' but you can call me ' + this.nickname);
+//   }
+// };
+// //Object Literal Notation -- is a form of making objects^^^^^^^^
+//
+// maddy.nickname;//dot notation
+// maddy['nickname'];//bracket notation
+
+
+// var seagullCount = [2, 38, 3 , 324, 4, 05];
+//
+// for (var i = 0; 1 < seagullCount.length; i++) {
+//   //1   Create the new Element
+//   //2   Give the element ome content
+//   //3   Append the element to the appripriate place in the document
+//   var LiEl = document.createElement('li')
+// }
+
+
+var storeHours = ['6-AM', '7-AM'];
+
+var pikePlaceMarket = {
+  locationName: 'Pike Place Market',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  averageCookiesSoldPerHour: 6.3,
+  calculateCustomersThisHour: function(Math.floor(math.random))/*use math.random formula*/ {},
+    customersThisHour: [],
+    calculateCookiesThisHour: function(Math.floor(math.random)) {},
+    cookiesEachHour: [],
+    render: function() {}
+}
+//dont forget to call the functions
+
+
+var Alki = {
+  locationName: 'Alki',
+  minCustomersPerHour: 4,
+  maxCustomersPerHour: 16,
+  averageCookiesSoldPerHour: 4.6,
+  calculateCustomersThisHour: function(Math.floor(math.random))/*use math.random formula*/ {},
+    customersThisHour: [],
+    calculateCookiesThisHour: function(Math.floor(math.random)) {},
+    cookiesEachHour: [],
+    render: function() {}
+}
+
+var Capitol Hill = {
+  locationName: 'Alki',
+  minCustomersPerHour: 4,
+  maxCustomersPerHour: 16,
+  averageCookiesSoldPerHour: 4.6,
+  calculateCustomersThisHour: function(Math.floor(math.random))/*use math.random formula*/ {},
+    customersThisHour: [],
+    calculateCookiesThisHour: function(Math.floor(math.random)) {},
+    cookiesEachHour: [],
+    render: function() {}
+}
+
+var Sea Tac Airport = {
+  locationName: 'Alki',
+  minCustomersPerHour: 4,
+  maxCustomersPerHour: 16,
+  averageCookiesSoldPerHour: 4.6,
+  calculateCustomersThisHour: function(Math.floor(math.random))/*use math.random formula*/ {},
+    customersThisHour: [],
+    calculateCookiesThisHour: function(Math.floor(math.random)) {},
+    cookiesEachHour: [],
+    render: function() {}
+}
+
+var Seattle Center = {
+  locationName: 'Alki',
+  minCustomersPerHour: 4,
+  maxCustomersPerHour: 16,
+  averageCookiesSoldPerHour: 4.6,
+  calculateCustomersThisHour: function(Math.floor(math.random))/*use math.random formula*/ {},
+    customersThisHour: [],
+    calculateCookiesThisHour: function(Math.floor(math.random)) {},
+    cookiesEachHour: [],
+    render: function() {}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//this is a push method
+
+var numbers = [3,4,5];
+var multipliedNumbers = [];
+
+
+function timesNine() {
+  for (var i = 0; i < numbers.length; i++) {
+    multipliedNumbers.push(numbers[i] * 9);
+  }
+}
+
+
+timesNine();
+console.log(multipliedNumbers);
